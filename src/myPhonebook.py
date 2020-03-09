6d09a3aef4fd6a7654984842a18e4e7b4f9ff6aa
daximillian <nofars@gmail.com>
2020-03-09 21:29:15 +0200

    change port

--- src/myPhonebook.py
+++ src/myPhonebook.py
@@ -121,7 +121,7 @@ def metrics():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
 
 cur.close()
 conn.close()
