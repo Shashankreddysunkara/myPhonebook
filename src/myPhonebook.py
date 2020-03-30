fdbc6dcfe6acfd88d71ae9d259b50fc31999388a
daximillian <nofars@gmail.com>
2020-03-30 13:11:49 +0300

    update with logging and parms

--- src/myPhonebook.py
+++ src/myPhonebook.py
@@ -3,7 +3,28 @@
 from prometheus_client import generate_latest, REGISTRY, Counter, Summary
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import logging
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--username', default='phoneapp')
+parser.add_argument('-psw', '--password', default='123456')
+parser.add_argument('-h', '--host', default='mysql.service.consul')
+parser.add_argument('-p', '--port', default='3306')
+parser.add_argument('-db', '--database', default='phonebook')
+parser.add_argument('-l', '--log', default='info', choices=['debug', 'info', 'warning'])
+
+args = parser.parse_args()
+
+# log config
+logging.basicConfig(format='%(asctime)s: %(message)s')
+logger = logging.getLogger("ExtractDeviceMetrics")
+if args.log == "info":
+    logger.setLevel(logging.INFO)
+elif args.log == "debug":
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.WARNING)
 
 # A counter to count the total number of HTTP requests
 REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', 
@@ -15,10 +36,12 @@ TIME_METRIC = Summary('http_request_processing_seconds',
 app = Flask("MyApp")
 
 conn = mysql.connector.connect(
-         user='phoneapp',
-         password='123456',
-         host='10.0.3.27',
-         database='phonebook')
+         user=args.username,
+         password=args.password,
+         host=args.host,
+         port=args.port,
+         database=args.database)
+
 #opens connection
 cur = conn.cursor()
 activeTab = {"home":"", "addEntries":"", "updateEntry":""}
@@ -30,13 +53,16 @@ listings_timer = TIME_METRIC.labels(method="listings")
 def listings():
     REQUESTS.labels(method='GET', endpoint="/", status_code=200).inc() 
     query = ("SELECT id,name,email,phone FROM phonebook")
-    cur.execute(query)
-    l = []
-    for each in cur.fetchall():
-        l.append(list(each))
-    setActiveTab("home")
-    return render_template("listings.html", contact_list=l, title="My Phonebook", activeTab=activeTab)
-
+    try:
+        cur.execute(query)
+        logger.info("Listing all database info")
+        l = []
+        for each in cur.fetchall():
+            l.append(list(each))
+        setActiveTab("home")
+        return render_template("listings.html", contact_list=l, title="My Phonebook", activeTab=activeTab)
+    except mysql.connector.Error as err:
+        logging.error(err)
 
 updateEntry_timer = TIME_METRIC.labels(method="updateEntry")
 @app.route("/update_entry")
@@ -45,10 +71,14 @@ def updateEntry(methods=['GET']):
     REQUESTS.labels(method='GET', endpoint="/update_entry", status_code=200).inc()
     id = request.args.get('id')
     query = ("SELECT id,name,email,phone FROM phonebook WHERE id = '%s'" % id)
-    cur.execute(query)
-    list = cur.fetchone()
-    setActiveTab("updateEntry")
-    return render_template("update_entry.html", phonebook=list, title="My Phonebook", activeTab=activeTab)
+    try:
+        cur.execute(query)
+        list = cur.fetchone()
+        setActiveTab("updateEntry")
+        logger.info("Selecting data to update")
+        return render_template("update_entry.html", phonebook=list, title="My Phonebook", activeTab=activeTab)
+    except mysql.connector.Error as err:
+        logging.error(err)
 
 # set all active tab dictionaries value to "" then make the indicated tab from the flask route function active. 
 # This make class="active" for css styling dynamically.
@@ -58,6 +88,7 @@ def setActiveTab(tabName):
         activeTab[tab] = ""
     if tabName in activeTab:
         activeTab[tabName] = "active"
+    logger.debug("Active tab is: {}".format(activeTab))
     print(activeTab)
 
 new_entry_timer = TIME_METRIC.labels(method="new_entry")
@@ -82,14 +113,21 @@ def submit_new_student():
         email = request.form.get('email')
         phone = request.form.get('phone')
         query = ("SELECT MAX(id) FROM phonebook")
-        cur.execute(query)
-        records = cur.fetchone()
-        newid = int(records[0]) + 1
-        query = "insert into phonebook (id, name, email, phone) values ({},'{}','{}','{}')".format(newid, name, email, phone)
-        cur.execute(query)
-        conn.commit()
-        lastId = cur.lastrowid
-
+        try:
+            cur.execute(query)
+            records = cur.fetchone()
+            newid = int(records[0]) + 1
+            logger.debug("New id is: {}".format(str(newid)))
+            query = "insert into phonebook (id, name, email, phone) values ({},'{}','{}','{}')".format(newid, name, email, phone)
+            try:
+                cur.execute(query)
+                conn.commit()
+                lastId = cur.lastrowid
+                logger.info("Created a new student - id: {}, name: {}, email: {}, phone: {}".format(newid, name, email, phone))
+            except mysql.connector.Error as err:
+                logging.error(err)    
+        except mysql.connector.Error as err:
+            logging.error(err)
     # return render_template("submit_new_entry.html", name=name,email=email,phone=phone, activeTab=activeTab)
     return redirect("/")
 
@@ -106,12 +144,20 @@ def submit_update_contact():
     phone = request.form.get('phone')
     if request.form['submit'] == 'update':
         query = "UPDATE phonebook SET name = '%s', email = '%s', phone = '%s' WHERE id = '%s'" % (name,email,phone, id)
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+            conn.commit()
+            logger.info("Update a student - id: {}, name: {}, email: {}, phone: {}".format(id, name, email, phone))
+        except mysql.connector.Error as err:
+            logging.error(err)
     if request.form['submit'] == 'delete':
         query = "DELETE FROM phonebook WHERE id = '%s'" % id
-        cur.execute(query)
-        conn.commit()
+        logger.info("Deleting student - id: {}".format(id))
+        try:
+            cur.execute(query)
+            conn.commit()
+        except mysql.connector.Error as err:
+            logging.error(err)
     return redirect("/")
 
 @app.route('/metrics')
